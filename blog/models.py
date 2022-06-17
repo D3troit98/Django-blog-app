3b4a5c76b507e92eaefa6880afac8846b70239e6
D3troit98 <duruakuebuka@gmail.com>
2022-06-17 23:26:36 +0100

    Django models

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+# Create your models here.
+User = get_user_model()
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey(User,on_delete=models.CASCADE,default=None)
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
